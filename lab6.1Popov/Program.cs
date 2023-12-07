cea86dea851e375fc294bb4a3da57f90e8915266
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-12-07 23:46:57 +0200

    Добавьте файлы проекта.

---
+++ lab6.1Popov/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.Write("Введите число:");
+    int n = int.Parse(Console.ReadLine());
+    int largDigit = FindLargDigit(n);
+    Console.Write("самое большое число: " + largDigit);
+    int FindLargDigit(int num)
+    {
+        if (num < 10);
+
+        else
+        {
+            int LastDigit = num % 10;
+            int RemDigit = num / 10;
+            int MaxRem = FindLargDigit(RemDigit);
+            return Math.Max(LastDigit, MaxRem);
+        }
+
+        {
+            return num;
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
